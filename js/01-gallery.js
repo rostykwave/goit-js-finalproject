3c33f4424be9a94c7c8ba213dfeed850084225a6
Rostyslav Kovalchuk <rostykwave@gmail.com>
2023-10-18 19:40:07 +0300

    01-gallery: add basic render of HTML

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><img src="${preview}" alt="${description}" class="gallery__image"></li>`
+  )
+  .join("");
 
+gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
