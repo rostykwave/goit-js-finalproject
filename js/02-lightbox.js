39e36ed84190dd25099e515d64bf0a5b6b4a7249
Rostyslav Kovalchuk <rostykwave@gmail.com>
2023-10-19 21:59:51 +0300

    02-lightbox: add SimpleLightbox logic and render g

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+////Creating a markup from the gallery-items file
+const markup = makeMarkup(galleryItems);
+////Inserting a markup
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+///initiation and modification of the lightbox signature
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function makeMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join("");
+}
